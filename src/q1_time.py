7d6d15fd9c9fb369935beb0933750e03503b4973
DuvanEZ <duvanminos@gmail.com>
2024-07-10 18:26:24 -0500

    Adding terraform to the project the credentials where change to avoid problems

--- src/q1_time.py
+++ src/q1_time.py
@@ -3,25 +3,37 @@ from datetime import datetime
 import pandas as pd
 import json
 
-def q1_time(file_path: str) -> List[Tuple[datetime.date, str, int]]:
+def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
+    # Abrir y leer el archivo JSON línea por línea
     with open(file_path, 'r') as json_file:
-        #Cargar archivo json linea a linea
         data = [json.loads(line.strip()) for line in json_file]
-    #Usar una list comprenhension para extraer los campos
-    data = [(item['date'], item['user']['username'], item['id']) for item in data if 'date' in item and 'user' in item and 'id' in item and 'id' in item['user']]
 
-    #Convertir la lista de tuplas en un dataframe de pandas
+    # Extraer los campos necesarios si están presentes en el item
+    data = [
+        (item['date'], item['user']['username'], item['id']) 
+        for item in data 
+        if 'date' in item and 'user' in item and 'id' in item and 'username' in item['user']
+    ]
+
+    # Crear un DataFrame de pandas con los datos extraídos
     df = pd.DataFrame(data, columns=['date', 'user', 'id'])
     
-    # Convertir campo date en formato datetime
+    # Convertir el campo 'date' a formato datetime.date
     df['date'] = pd.to_datetime(df['date']).dt.date
 
+    # Agrupar por fecha y contar los tweets, luego seleccionar las 10 fechas con más tweets
     tweet_counts = df.groupby('date').size()
-    top_10_dates = tweet_counts.nlargest(10).index
-    df_top_10 = df[df['date'].isin(top_10_dates)]
-    top_users = df_top_10.groupby('date')['user'].agg(lambda x: x.value_counts().index[0])
+    top_10_dates = tweet_counts.nlargest(10).index.tolist()
 
-    # Convertir el resulatado en una lista de tuplas
-    result = [(date, user) for date, user in zip(top_10_dates, top_users)]
+    # Encontrar el usuario más frecuente para cada una de las 10 fechas con más tweets
+    top_users = (
+        df[df['date'].isin(top_10_dates)]
+        .groupby('date')['user']
+        .agg(lambda x: x.mode()[0] if not x.empty else None)
+        .reindex(top_10_dates)  # Asegurar el orden correcto de las fechas
+    )
+
+    # Convertir el resultado en una lista de tuplas (fecha, usuario más frecuente)
+    result = list(top_users.items())
     
     return result
