b759ee9614fbb0ff7d94bdc47cc707e65ce9a171
DuvanEZ <duvanminos@gmail.com>
2024-07-10 13:47:56 -0500

    Adding the modifications on the dockerfile and creating the flask app

--- main.py
+++ main.py
@@ -1,103 +1,74 @@
-import streamlit as st
-import pandas as pd
-import plotly.graph_objects as go
-from memory_profiler import memory_usage
-#from src.q1_memory import q1_memory
-#from src.q1_time import q1_time
-from src.q2_memory import q2_memory
+from flask import Flask, jsonify, request, current_app
+import os
+from src.q1_time import q1_time
+from src.q1_memory import q1_memory
 from src.q2_time import q2_time
-from src.q3_memory import q3_memory
+from src.q2_memory import q2_memory
 from src.q3_time import q3_time
+from src.q3_memory import q3_memory
+import logging
+import pandas as pd
 
-def main():
-    file_path = r"\Users\DZWorld2\Documents\Studying\challenge_option\tweets.json\farmers-protest-tweets-2021-2-4.json"
+app = Flask(__name__)
+print("Started.")
 
-    # Prepare data for q1 and q2 without measuring execution time and memory usage
-    #q1time = q1_time(file_path)
-    #q1memory = q1_memory(file_path)
-    q2time = q2_time(file_path)
-    q2memory = q2_memory(file_path)
-    q3time = q3_time(file_path)
-    q3memory = q3_memory(file_path)
+@app.route('/')
+def home():
+    return '''
+    <h1>Welcome to the Data Processing API</h1>
+   <p>Con esta aplicación hecha en Flask se pretende mostrar los resultados a las preguntas específicas. Para cada pregunta se elaboraron dos soluciones: una memory efficient y otra time efficient. Para ver el detalle del código, por favor ir a este repositorio en GitHub <a href="https://github.com/DuvanEZ/latam-challenge" target="_blank">https://github.com/DuvanEZ/latam-challenge</a>.</p>
 
-    #q1memory_df = pd.DataFrame(q1memory, columns=['Date', 'UserName', 'Count'])
-    #q1time_df = pd.DataFrame(q1time, columns=['Date', 'UserName'])
-    q2memory_df = pd.DataFrame(q2memory, columns=['Emoji', 'Count'])
-    q2time_df = pd.DataFrame(q2time, columns=['Emoji', 'Count'])
-    q3memory_df = pd.DataFrame(q3memory, columns=['Username', 'Count'])
-    q3time_df = pd.DataFrame(q3time, columns=['Username', 'Count'])
+    <button onclick="fetchAndDisplayResults()">Run Analysis</button>
+    <div id="resultsTable">Click the button to run analysis.</div>
+    <script>
+        function fetchAndDisplayResults() {
+            const resultsTable = document.getElementById('resultsTable');
+            resultsTable.innerHTML = 'Processing...';
 
-    # Sidebar for question selection
-    st.sidebar.title("Select Question")
-    question = st.sidebar.radio("Choose a question to view", ("Question 2", "Question 3"))
+            fetch('/run')
+                .then(response => response.json())
+                .then(data => {
+                    let tableHTML = '<table><tr><th>Function</th><th>Result</th></tr>';
+                    data.forEach(row => {
+                        tableHTML += `<tr><td>${row.function}</td><td>${row.result}</td></tr>`;
+                    });
+                    tableHTML += '</table>';
+                    resultsTable.innerHTML = tableHTML;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    resultsTable.innerHTML = 'An error occurred. Please try again.';
+                });
+        }
 
-    st.title("Twitter Data Analysis")
+    </script>
+    ''', 200
 
-    # Create separate containers for each table
-    container_q2_time = st.empty()
-    container_q2_memory = st.empty()
-    container_q3_time = st.empty()
-    container_q3_memory = st.empty()
+@app.route('/run', methods=['GET']) 
+def process_files():
+    with app.app_context():
+        # Set up logging
+        logging.basicConfig(filename='process_files.log', level=logging.INFO)
 
-    if question == "Question 2":
-        with container_q2_time.container():
-            st.header("Question 2")
-            st.subheader("Execution Time")
-            fig_q2_time = go.Figure(data=[go.Table(
-                header=dict(values=list(q2time_df.columns),
-                            fill_color='paleturquoise',
-                            align='left'),
-                cells=dict(values=[q2time_df[col] for col in q2time_df.columns],
-                           fill_color='lavender',
-                           align='left'))
-            ])
-            st.plotly_chart(fig_q2_time)
+        try:
+            logging.info("Starting functions...")
+            file_path = "farmers-protest-tweets-2021-2-4.json"
+            results = [
+                {'function': 'q1_time', 'result': q1_time(file_path)},
+                {'function': 'q1_memory', 'result': q1_memory(file_path)},
+                {'function': 'q2_time', 'result': q2_time(file_path)},
+                {'function': 'q2_memory', 'result': q2_memory(file_path)},
+                {'function': 'q3_time', 'result': q3_time(file_path)},
+                {'function': 'q3_memory', 'result': q3_memory(file_path)}
+            ]
+            df = pd.DataFrame(results)
+            print(df)
+            return jsonify(df.to_dict(orient='records')), 200
 
-        with container_q2_memory.container():
-            st.subheader("Memory Usage")
-            fig_q2_memory = go.Figure(data=[go.Table(
-                header=dict(values=list(q2memory_df.columns),
-                            fill_color='paleturquoise',
-                            align='left'),
-                cells=dict(values=[q2memory_df[col] for col in q2memory_df.columns],
-                           fill_color='lavender',
-                           align='left'))
-            ])
-            st.plotly_chart(fig_q2_memory)
+        except Exception as e:
+            logging.error(f"Error: {str(e)}")
+            return jsonify({'message': f'Error: {str(e)}'}), 500
 
-        # Clear the containers for question 3
-        container_q3_time.empty()
-        container_q3_memory.empty()
-
-    elif question == "Question 3":
-        with container_q3_time.container():
-            st.header("Question 3")
-            st.subheader("Execution Time")
-            fig_q3_time = go.Figure(data=[go.Table(
-                header=dict(values=list(q3time_df.columns),
-                            fill_color='paleturquoise',
-                            align='left'),
-                cells=dict(values=[q3time_df[col] for col in q3time_df.columns],
-                           fill_color='lavender',
-                           align='left'))
-            ])
-            st.plotly_chart(fig_q3_time)
-
-        with container_q3_memory.container():
-            st.subheader("Memory Usage")
-            fig_q3_memory = go.Figure(data=[go.Table(
-                header=dict(values=list(q3memory_df.columns),
-                            fill_color='paleturquoise',
-                            align='left'),
-                cells=dict(values=[q3memory_df[col] for col in q3memory_df.columns],
-                           fill_color='lavender',
-                           align='left'))
-            ])
-            st.plotly_chart(fig_q3_memory)
-
-        # Clear the containers for question 2
-        container_q2_time.empty()
-        container_q2_memory.empty()
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    print("Starting API on port 80.")
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 80)))
---
+++ streamlit.py
@@ -0,0 +1,137 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+from memory_profiler import memory_usage
+from src.q1_memory import q1_memory
+from src.q1_time import q1_time
+from src.q2_memory import q2_memory
+from src.q2_time import q2_time
+from src.q3_memory import q3_memory
+from src.q3_time import q3_time
+
+def main():
+    #file_path = r"\Users\DZWorld2\Documents\Studying\challenge_option\tweets.json\farmers-protest-tweets-2021-2-4.json"
+    file_path = "farmers-protest-tweets-2021-2-4.json"
+
+    # Prepare data for q1 and q2 without measuring execution time and memory usage
+    q1time = q1_time(file_path)
+    q1memory = q1_memory(file_path)
+    q2time = q2_time(file_path)
+    q2memory = q2_memory(file_path)
+    q3time = q3_time(file_path)
+    q3memory = q3_memory(file_path)
+
+    q1memory_df = pd.DataFrame(q1memory, columns=['Date', 'UserName', 'Count'])
+    q1time_df = pd.DataFrame(q1time, columns=['Date', 'UserName'])
+    q2memory_df = pd.DataFrame(q2memory, columns=['Emoji', 'Count'])
+    q2time_df = pd.DataFrame(q2time, columns=['Emoji', 'Count'])
+    q3memory_df = pd.DataFrame(q3memory, columns=['Username', 'Count'])
+    q3time_df = pd.DataFrame(q3time, columns=['Username', 'Count'])
+
+    # Sidebar for question selection
+    st.sidebar.title("Select Question")
+    question = st.sidebar.radio("Choose a question to view", ("Question 1","Question 2", "Question 3"))
+
+    st.title("Twitter Data Analysis")
+
+    # Create separate containers for each table
+    container_q1_time = st.empty()
+    container_q1_memory = st.empty()
+    container_q2_time = st.empty()
+    container_q2_memory = st.empty()
+    container_q3_time = st.empty()
+    container_q3_memory = st.empty()
+
+    if question == "Question 1":
+        with container_q1_time.container():
+            st.header("Question 1")
+            st.subheader("Execution Time")
+            fig_q1_time = go.Figure(data=[go.Table(
+                header=dict(values=list(q1time_df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[q1time_df[col] for col in q1time_df.columns],
+                           fill_color='lavender',
+                           align='left'))
+            ])
+            st.plotly_chart(fig_q1_time)
+
+        with container_q1_memory.container():
+            st.subheader("Memory Usage")
+            fig_q1_memory = go.Figure(data=[go.Table(
+                header=dict(values=list(q1memory_df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[q1memory_df[col] for col in q1memory_df.columns],
+                           fill_color='lavender',
+                           align='left'))
+            ])
+            st.plotly_chart(fig_q1_memory)
+
+        # Clear the containers for question 2 and 3
+        container_q2_time.empty()
+        container_q2_memory.empty()
+        container_q3_time.empty()
+        container_q3_memory.empty()
+    elif question == "Question 2":
+        with container_q2_time.container():
+            st.header("Question 2")
+            st.subheader("Execution Time")
+            fig_q2_time = go.Figure(data=[go.Table(
+                header=dict(values=list(q2time_df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[q2time_df[col] for col in q2time_df.columns],
+                           fill_color='lavender',
+                           align='left'))
+            ])
+            st.plotly_chart(fig_q2_time)
+
+        with container_q2_memory.container():
+            st.subheader("Memory Usage")
+            fig_q2_memory = go.Figure(data=[go.Table(
+                header=dict(values=list(q2memory_df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[q2memory_df[col] for col in q2memory_df.columns],
+                           fill_color='lavender',
+                           align='left'))
+            ])
+            st.plotly_chart(fig_q2_memory)
+
+        # Clear the containers for question 3
+        container_q3_time.empty()
+        container_q3_memory.empty()
+
+    elif question == "Question 3":
+        with container_q3_time.container():
+            st.header("Question 3")
+            st.subheader("Execution Time")
+            fig_q3_time = go.Figure(data=[go.Table(
+                header=dict(values=list(q3time_df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[q3time_df[col] for col in q3time_df.columns],
+                           fill_color='lavender',
+                           align='left'))
+            ])
+            st.plotly_chart(fig_q3_time)
+
+        with container_q3_memory.container():
+            st.subheader("Memory Usage")
+            fig_q3_memory = go.Figure(data=[go.Table(
+                header=dict(values=list(q3memory_df.columns),
+                            fill_color='paleturquoise',
+                            align='left'),
+                cells=dict(values=[q3memory_df[col] for col in q3memory_df.columns],
+                           fill_color='lavender',
+                           align='left'))
+            ])
+            st.plotly_chart(fig_q3_memory)
+
+        # Clear the containers for question 2
+        container_q2_time.empty()
+        container_q2_memory.empty()
+
+if __name__ == '__main__':
+    main()
